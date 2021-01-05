08e22afb7e3edecb813f635b1bdfca7d230d1ec5
Junsik Moon <jeru8603@outlook.kr>
2021-01-05 15:26:02 +0900

    commit

---
+++ C/VST/Proj_01/Proj_01/Test.c
@@ -0,0 +1,20 @@
+/*
+Python Code
+
+print("Hello, World!")
+
+print() -> 화면에 괄호 안에 있는 문자를 출력해달라고 요청하는 함수
+
+*/
+
+/*
+C Code
+*/
+#include <stdio.h>
+
+int main() 
+{
+	printf("Hello, World!");
+
+	return 0;
+}
